2f8cd12d34a91b6494f8b7609a83d7c494272823
Dave Smith <smith@wiresareobsolete.com>
2017-03-12 22:21:01 -0600

    Add profile detail. Move data to top scope.

--- public/js/admin-app.js
+++ public/js/admin-app.js
@@ -49,13 +49,52 @@ app.factory("Avatar", ["$firebaseStorage",
 ]);
 
 /* Controller to manage user login */
-app.controller("AuthCtrl", function($scope, $firebaseAuth, Config) {
+app.controller("AuthCtrl", function($scope, $firebaseAuth, $firebaseObject, $firebaseArray, Config) {
   // add config parameters
   $scope.config = Config();
   $scope.validAdminUser = false;
   // Set the default selected tab
   $scope.tabs = {};
   $scope.tabs.selectedIndex = 0;
+
+  // create an instance of the authentication service
+  var auth = $firebaseAuth();
+  auth.$onAuthStateChanged(function(firebaseUser) {
+    if (firebaseUser == null) return;
+
+    var profileRef = firebase.database().ref(PROFILE_URL);
+    var profileQuery = profileRef.orderByChild("name");
+    var submissionRef = firebase.database().ref(SUBMISSION_URL);
+    var submissionQuery = submissionRef.orderByChild("title");
+
+    // Fetch firebase data
+    $scope.profiles = $firebaseObject(profileRef);
+    $scope.profilesList = $firebaseArray(profileQuery)
+    $scope.submissions = $firebaseArray(submissionQuery);
+    // Compute reviewer data
+    $scope.scores = {};
+    $scope.submissions.$loaded().then(function() {
+      angular.forEach($scope.submissions, function(submission) {
+        ComputeReviewScore(submission, $scope.scores);
+      })
+    });
+  });
+
+  function ComputeReviewScore(item, scores) {
+    //Set the default value
+    scores[item.$id] = 0.0;
+    if (item.scores) {
+      var count = Object.keys(item.scores).length;
+      if (count > 0) {
+        var sum = 0.0;
+        for (var key in item.scores) {
+          sum += item.scores[key];
+        }
+
+        scores[item.$id] = sum / count;
+      }
+    }
+  }
 
   // login button function
   $scope.loginUser = function() {
@@ -85,21 +124,46 @@ app.controller("AuthCtrl", function($scope, $firebaseAuth, Config) {
 });
 
 /* Controller to list and manage speaker profiles */
-app.controller("SpeakerCtrl", function($scope, $firebaseAuth, $firebaseArray) {
-  // create an instance of the authentication service
-  var auth = $firebaseAuth();
-  auth.$onAuthStateChanged(function(firebaseUser) {
-    if (firebaseUser == null) return;
+app.controller("ProfileCtrl", function($scope, $firebaseArray, $mdDialog) {
 
-    var ref = firebase.database().ref(PROFILE_URL);
-    var query = ref.orderByChild("name");
+  $scope.showProfileDetail = function(evt, profileItem, avatarUrl) {
+    ShowProfileDialog(evt, profileItem, avatarUrl);
+  }
 
-    $scope.profiles = $firebaseArray(query);
-  });
+  function ShowProfileDialog(evt, profileItem, avatarUrl) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'profile.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: evt,
+      clickOutsideToClose:true,
+      fullscreen: true,
+      locals: {
+        speaker: profileItem,
+        imageUrl: avatarUrl
+      }
+    })
+    .then(function() {
+      // Dialog cancelled
+    });
+  }
 
-  $scope.twitterUrl = function(twitterHandle) {
-    return twitterHandle.replace('@', 'https://twitter.com/');
-  };
+  // Handler for entry dialog events
+  function DialogController($scope, $mdDialog, speaker, imageUrl) {
+    $scope.speaker = speaker;
+    $scope.imageUrl = imageUrl;
+    $scope.twitterUrl = function(twitterHandle) {
+      return twitterHandle.replace('@', 'https://twitter.com/');
+    };
+
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.close = function() {
+      $mdDialog.hide();
+    };
+  }
 });
 
 app.controller("ProfileItemCtrl", function($scope, Avatar) {
@@ -116,7 +180,7 @@ app.controller("ProfileItemCtrl", function($scope, Avatar) {
 });
 
 /* Controller to list and manage submission items */
-app.controller("SubmissionCtrl", function($scope, $firebaseAuth, $firebaseObject, $firebaseArray, $mdDialog) {
+app.controller("SubmissionCtrl", function($scope, $firebaseObject, $firebaseArray, $mdDialog) {
   $scope.sortOptions = [
     {value: 'title', label: "Session Title"},
     {value: 'name', label: "Speaker Name"},
@@ -137,43 +201,6 @@ app.controller("SubmissionCtrl", function($scope, $firebaseAuth, $firebaseObject
       default:
         $scope.reverseSort = false;
         return item.title;
-    }
-  }
-
-  // create an instance of the authentication service
-  var auth = $firebaseAuth();
-  auth.$onAuthStateChanged(function(firebaseUser) {
-    if (firebaseUser == null) return;
-
-    var profileRef = firebase.database().ref(PROFILE_URL);
-    var submissionRef = firebase.database().ref(SUBMISSION_URL);
-    var query = submissionRef.orderByChild("title");
-
-    // Fetch firebase data
-    $scope.profiles = $firebaseObject(profileRef);
-    $scope.submissions = $firebaseArray(query);
-    // Compute reviewer data
-    $scope.scores = {};
-    $scope.submissions.$loaded().then(function() {
-      angular.forEach($scope.submissions, function(submission) {
-        ComputeReviewScore(submission, $scope.scores);
-      })
-    });
-  });
-
-  function ComputeReviewScore(item, scores) {
-    //Set the default value
-    scores[item.$id] = 0.0;
-    if (item.scores) {
-      var count = Object.keys(item.scores).length;
-      if (count > 0) {
-        var sum = 0.0;
-        for (var key in item.scores) {
-          sum += item.scores[key];
-        }
-
-        scores[item.$id] = sum / count;
-      }
     }
   }
 
